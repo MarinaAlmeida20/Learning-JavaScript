54c155ada8f711ff20e51901412369914e281733
Marina <marinabrito.ma@gmail.com>
2021-01-30 11:27:20 +0000

    return

--- index.js
+++ index.js
@@ -10,37 +10,36 @@ class ContaCorrente {
     sacar(valor){
         if(this._saldo >= valor){
             this._saldo -= valor;
+            return valor;
         }
     }
 
     depositar(valor){
         if(valor > 0){
-            this._saldo += valor;
+            return;
         }
+        this._saldo += valor;
     }
 }
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
 
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
+
+const cliente2 = new Cliente();
 
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
-
 
 const contaCorrenteRicardo = new ContaCorrente();
 
-
-contaCorrenteRicardo.saldo = 0
 contaCorrenteRicardo.agencia = 1001;
 
 contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.sacar(50);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(-100);
+const valorSacado = contaCorrenteRicardo.sacar(50);
 
+console.log(valorSacado);
 console.log(contaCorrenteRicardo);
